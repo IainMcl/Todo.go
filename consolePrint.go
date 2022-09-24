7a40f28ec8f2863ba84a55546c6dd48b32788307
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2022-09-24 17:38:47 +0100

    Nicer printing.

--- consolePrint.go
+++ consolePrint.go
@@ -36,31 +36,33 @@ func consoleSize() (int, int, bool) {
 
 func NewConsolePrint() *ConsolePrint {
 	w, h, correctSize := consoleSize()
-	wId := 4
+	wId := 5
 	wPriority := 9
 	wCompleted := 10
 
-	remainingWidth := w - wId - wPriority - wCompleted - 13
-	wName := remainingWidth / 3
-	wContent := remainingWidth * 2 / 3
-
-	totalWidth := wId + wName + wContent + wPriority + wCompleted + 13
-	// If the division leave a remainder, add one to the content width
-	if totalWidth != w {
-		// fmt.Println("Width calculation error: ", totalWidth, w)
-		wContent += w - totalWidth
+	remainingWidth := w - wId - wPriority - wCompleted
+	wName := remainingWidth / 4
+	if wName < 20 {
+		wName = 20
 	}
+
+	totalWidth := wId + wName + wPriority + wCompleted + 5 // 5 is for the 4 spaces and the border
+	wContent := w - totalWidth
 
 	return &ConsolePrint{
 		width:  w,
 		height: h,
 		colour: map[string]string{
-			"border":  "\033[34m",
-			"error":   "\033[31m",
-			"warning": "\033[33m",
-			"success": "\033[32m",
-			"null":    "\033[30m",
-			"white":   "\033[37m",
+			"border":    "\033[34m",
+			"error":     "\033[31m",
+			"warning":   "\033[33m",
+			"success":   "\033[32m",
+			"null":      "\033[37m",
+			"white":     "\033[37m",
+			"bold":      "\033[1m",
+			"italic":    "\033[3m",
+			"underline": "\033[4m",
+			"normal":    "\033[0m",
 		},
 		idWidth:        wId,
 		priorityWidth:  wPriority,
@@ -72,19 +74,25 @@ func NewConsolePrint() *ConsolePrint {
 }
 
 func (c ConsolePrint) printHeader() {
-	fmt.Println(c.colour["border"], strings.Repeat("=", c.width-2))
+	c.printHeaderDivider()
 	// Id | Priority | Completed are fixed width elements. Name and content are variable width
 	// Name and content should wrap to next line if they are too long
 	// Print the column names for the table
-	fmt.Println("|",
+	fmt.Print("| ",
 		c.colour["white"],
-		"ID", strings.Repeat(" ", c.idWidth-2),
+		c.colour["bold"],
+		"ID", strings.Repeat(" ", c.idWidth),
 		"Name", strings.Repeat(" ", c.nameWidth-4),
 		"Content", strings.Repeat(" ", c.contentWidth-7),
 		"Priority", strings.Repeat(" ", c.priorityWidth-8),
 		"Completed",
+		c.colour["normal"],
 		c.colour["border"],
-		"|")
+		" |\n")
+	c.printHeaderDivider()
+}
+
+func (c ConsolePrint) printHeaderDivider() {
 	fmt.Println(c.colour["border"], strings.Repeat("=", c.width-2))
 }
 
@@ -94,6 +102,25 @@ func (c ConsolePrint) printFooter() {
 
 func (c ConsolePrint) printDivider() {
 	fmt.Println(c.colour["border"], strings.Repeat("-", c.width-2))
+}
+
+func getLineContent(content string, width int, lineNumber int) string {
+	var nameString string
+	if lineNumber <= width && len(content) > width {
+		if width > len(content[width*lineNumber:]) {
+			nameString = content[lineNumber*width:]
+		} else {
+			nameString = content[lineNumber*width : (lineNumber+1)*width]
+		}
+	} else if lineNumber == 0 {
+		nameString = content
+	} else {
+		nameString = ""
+	}
+	if nameString != "" && nameString[0] == ' ' {
+		nameString = nameString[1:]
+	}
+	return nameString
 }
 
 func (c ConsolePrint) printTodo(t todo) {
@@ -114,53 +141,32 @@ func (c ConsolePrint) printTodo(t todo) {
 		nWraps = contentWraps
 	}
 
-	// fmt.Println("Number of wraps: ", nWraps)
-
 	for i := 0; i <= nWraps; i++ {
-		var nameString string
-		if i <= nameWraps && len(t.name) > c.nameWidth {
-			if c.nameWidth > len(t.name[c.nameWidth*i:]) {
-				nameString = t.name[i*c.nameWidth:]
-			} else {
-				nameString = t.name[i*c.nameWidth : (i+1)*c.nameWidth]
-			}
-		} else if i == 0 {
-			nameString = t.name
-		} else {
-			nameString = strings.Repeat(" ", c.nameWidth)
-		}
-		var contentString string
-		if i <= contentWraps && len(t.content) > c.contentWidth {
-			if c.contentWidth > len(t.content[c.contentWidth*i:]) {
-				contentString = t.content[i*c.contentWidth:]
-			} else {
-				contentString = t.content[i*c.contentWidth : (i+1)*c.contentWidth]
-			}
-		} else if i == 0 {
-			contentString = t.content
-		} else {
-			contentString = strings.Repeat(" ", c.contentWidth)
-		}
+
+		nameString := getLineContent(t.name, c.nameWidth, i)
+		contentString := getLineContent(t.content, c.contentWidth, i)
+
 		if i == 0 {
-			fmt.Println("|",
+			fmt.Print("| ",
 				c.colour["white"],
-				t.id, strings.Repeat(" ", c.idWidth-len(strconv.FormatInt(int64(t.id), 10))),
+				t.id, strings.Repeat(" ", c.idWidth-len(strconv.FormatInt(int64(t.id), 10))+2),
 				nameString, strings.Repeat(" ", c.nameWidth-len(nameString)),
-				contentString, strings.Repeat(" ", c.contentWidth-len(contentString)+7),
-				t.priority, strings.Repeat(" ", c.priorityWidth-len(strconv.FormatInt(int64(t.priority), 10))+1),
+				contentString, strings.Repeat(" ", c.contentWidth-len(contentString)),
+				strings.Repeat(" ", len("priority")),
+				t.priority, strings.Repeat(" ", c.priorityWidth-len(strconv.FormatInt(int64(t.priority), 10))),
 				check,
 				c.colour["border"],
-				"|")
+				" |")
 		} else {
-			fmt.Println("|",
+			fmt.Print("| ",
 				c.colour["white"],
-				strings.Repeat(" ", c.idWidth+len(strconv.FormatInt(int64(t.id), 10))),
+				strings.Repeat(" ", c.idWidth+2),
 				nameString, strings.Repeat(" ", c.nameWidth-len(nameString)),
 				contentString, strings.Repeat(" ", c.contentWidth-len(contentString)),
 				strings.Repeat(" ", c.priorityWidth),
-				strings.Repeat(" ", c.completedWidth),
+				strings.Repeat(" ", c.completedWidth-1),
 				c.colour["border"],
-				"|")
+				" |")
 		}
 	}
 }
